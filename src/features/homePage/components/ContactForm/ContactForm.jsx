cbea18cedbb1e4cab0212e29452b9c807255812a
Ahmad Ghazzi <aghazzi.1998@gmail.com>
2023-05-31 15:11:49 +0300

    fixed responsive design

--- src/features/homePage/components/ContactForm/ContactForm.jsx
+++ src/features/homePage/components/ContactForm/ContactForm.jsx
@@ -77,63 +77,65 @@ export const ContactForm = () => {
                     support
                 </h2>
             </div>
-            <Form
-                // id="contact--form"
-                // ref={contactForm}
-                layout="vertical"
-                name="nest-messages"
-                form={form}
-                onFinish={onFinish}
-                style={{
-                    width: "400px",
-                }}
-                validateMessages={validateMessages}
-            >
-                <Item
-                    name="name"
-                    label="Name"
-                    rules={[
-                        {
-                            required: true,
-                        },
-                    ]}
-                >
-                    <Input />
-                </Item>
-                <Item
-                    name="email"
-                    label="Email"
-                    rules={[
-                        {
-                            required: true,
-                            type: "email",
-                        },
-                    ]}
-                >
-                    <Input />
-                </Item>
-                <Item
-                    name="message"
-                    label="Message"
-                    rules={[
-                        {
-                            required: true,
-                        },
-                    ]}
-                >
-                    <Input.TextArea />
-                </Item>
-                <Item
-                    wrapperCol={{
-                        ...layout.wrapperCol,
-                        offset: 8,
+            <div className="contactform-form">
+                <Form
+                    // id="contact--form"
+                    // ref={contactForm}
+                    layout="vertical"
+                    name="nest-messages"
+                    form={form}
+                    onFinish={onFinish}
+                    style={{
+                        width: "400px",
                     }}
+                    validateMessages={validateMessages}
                 >
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Item>
-            </Form>
+                    <Item
+                        name="name"
+                        label="Name"
+                        rules={[
+                            {
+                                required: true,
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Item>
+                    <Item
+                        name="email"
+                        label="Email"
+                        rules={[
+                            {
+                                required: true,
+                                type: "email",
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Item>
+                    <Item
+                        name="message"
+                        label="Message"
+                        rules={[
+                            {
+                                required: true,
+                            },
+                        ]}
+                    >
+                        <Input.TextArea />
+                    </Item>
+                    <Item
+                        wrapperCol={{
+                            ...layout.wrapperCol,
+                            offset: 8,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Item>
+                </Form>
+            </div>
         </div>
     );
 };
